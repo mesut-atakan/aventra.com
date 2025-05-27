2e1702763cc4f179a7d210f6fd7b18b403dbe566
mesut_atakan <mesut_atakan5@hotmail.com>
2025-05-27 22:47:00 +0300

    (authentication)

--- forum/auth-guard.js
+++ forum/auth-guard.js
@@ -1,7 +1,7 @@
+import { app } from './firebase-init.js';
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-
 export function requireAuth(redirectUrl = "login.html") {
-    const auth = getAuth();
+    const auth = getAuth(app);
     onAuthStateChanged(auth, user => {
         if (!user) {
             window.location.href = redirectUrl;
---
+++ forum/firebase-init.js
@@ -0,0 +1,12 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+const firebaseConfig = {
+      apiKey: "AIzaSyAfkPiGwLZwvPQcTCFaQu-he7NjS4CG27U",
+      authDomain: "aventra-platform.firebaseapp.com",
+      projectId: "aventra-platform",
+      storageBucket: "aventra-platform.appspot.com",
+      messagingSenderId: "1044485055341",
+      appId: "1:1044485055341:web:0bd3b3a2cecdded9423064",
+      measurementId: "G-RZJ4H09WJW"
+};
+export const app = initializeApp(firebaseConfig);
