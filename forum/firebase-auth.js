7bc76d992bd04707e14778a57b512c11de1a7ba7
mesut_atakan <mesut_atakan5@hotmail.com>
2025-05-30 12:01:07 +0300

    (user) wip : login and register

--- forum/firebase-auth.js
+++ forum/firebase-auth.js
@@ -12,3 +12,25 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export { createUserWithEmailAndPassword, signInWithEmailAndPassword };
+
+import { db } from './firebase-init.js';
+import { doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { auth } from './firebase-auth.js';
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    // users koleksiyonunda var mý kontrol et
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
+    if (!userSnap.exists()) {
+      // kullanýcýyý ekle (veya güncelle)
+      await setDoc(userRef, {
+        uid: user.uid,
+        email: user.email,
+        username: user.displayName || user.email.split("@")[0],
+        photoURL: user.photoURL || "/assets/images/default-profile-photo.jpg"
+      });
+    }
+  }
+});
